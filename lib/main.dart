a6ee95f6f5a58722d6dea004b832f63477ff1f8e
Roxanne Reyes <waetechsolutions@gmail.com>
2020-05-13 13:46:15 -0400

    Built a custom list, styled texts on transaction container

--- lib/main.dart
+++ lib/main.dart
@@ -58,8 +58,50 @@ class MyApp extends StatelessWidget {
               ),
             ),
           ),
-          Card(child: Text('List of expenses'),),
-        ],
+          Column(children: transactions.map((tx) {
+            return Card(
+              child: Row(
+                children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(
+                      vertical: 10,
+                  horizontal: 15,
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 2,
+                    ),
+                  ),
+                  padding: EdgeInsets.all(10),
+                  child: Text(tx.amount.toString(),
+                    style: TextStyle(fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple,
+
+                    ),
+                ),
+                ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(tx.title,
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      Text(tx.date.toString(),
+                        style: TextStyle(color: Colors.grey[800],
+                        ),
+                      ),
+                    ],),
+                  ],
+                )
+            );
+          }).toList(),
+          ),
+          ],
         ),
       );
     }
